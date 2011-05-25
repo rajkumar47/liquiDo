f5bf4d679b973c2cffe0a9fc427ab4cdf4d6989e
Raj <rajkumar.projects.db@gmail.com>
2011-05-25 15:58:09 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LiquiDo::Application.config.secret_token = '2ebe55fdd93a11ae6a5d2c18efc1622bbd6729040215c90793c4b1cd1a8252c4ca03c98168a4a6da93a2568fc38e3792d15097904dd32119578d678aa18f781f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LiquiDo::Application.config.session_store :cookie_store, :key => '_liquiDo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LiquiDo::Application.config.session_store :active_record_store
