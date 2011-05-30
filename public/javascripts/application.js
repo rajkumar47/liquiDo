d9109ec1e77f982b601588eae1dd117f8fc5d6f5
Raj Kumar <rajkumar.rajendran.work@gmail.com>
2011-05-29 21:18:01 -0400

    javascript done

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,9 +5,87 @@ $(document).ready( function() {
   $("#tabs").tabs();
   $("#accordion").accordion();
   $("#sortable").sortable();
-  $("#draggable").draggable({
-    connectToSortable:  "#sortable",
+  $(".draggable").draggable({
+    connectToSortable:  ".sortable",
     helper:             "clone",
     revert:             "invalid"
+  });
+  //-----------
+  // -
+  $("#info").droppable({
+    accept: ".data",
+    drop:   function( event, ui ){
+      var MAX_INFO_FIELDS = 7;
+      var n = $( this ).find("li").length;
+      if (n < MAX_INFO_FIELDS ){
+        $("<li></li>").text( ui.draggable.text() ).appendTo( this);
+        if ( n == (MAX_INFO_FIELDS - 1)){ $(this).find("#add").remove();}
+      } // if
+    }   // drop 
+  });
+
+  // -
+  $("#icon1").droppable({
+    accept: ".icons",
+    drop:   function( event, ui ){
+      var MAX_INFO_FIELDS = 1;
+      var n = $( this ).find("li").length;
+      if (n < MAX_INFO_FIELDS ){
+        $("<li></li>").text( ui.draggable.text() ).appendTo( this);
+        if ( n == (MAX_INFO_FIELDS - 1)){ $(this).find("#add").remove();}
+      } // if
+    }   // drop 
+  });
+
+  // -
+  $("#icon2").droppable({
+    accept: ".icons",
+    drop:   function( event, ui ){
+      var MAX_INFO_FIELDS = 1;
+      var n = $( this ).find("li").length;
+      if (n < MAX_INFO_FIELDS ){
+        $("<li></li>").text( ui.draggable.text() ).appendTo( this);
+        if ( n == (MAX_INFO_FIELDS - 1)){ $(this).find("#add").remove();}
+      } // if
+    }   // drop 
+  });
+
+  // -
+  $("#comment").droppable({
+    accept: ".comments",
+    drop:   function( event, ui ){
+      var MAX_INFO_FIELDS = 1;
+      var n = $( this ).find("li").length;
+      if (n < MAX_INFO_FIELDS ){
+        $("<li></li>").text( ui.draggable.text() ).appendTo( this);
+        if ( n == (MAX_INFO_FIELDS - 1)){ $(this).find("#add").remove();}
+      } // if
+    }   // drop 
+  });
+
+  $("#doc").droppable({
+    accept: ".docs",
+    drop:   function( event, ui ){
+      var MAX_INFO_FIELDS = 1;
+      var n = $( this ).find("li").length;
+      if (n < MAX_INFO_FIELDS ){
+        $("<li></li>").text( ui.draggable.text() ).appendTo( this);
+        if ( n == (MAX_INFO_FIELDS - 1)){ $(this).find("#add").remove();}
+      } // if
+    }   // drop 
+  });
+
+  //-
+  //-
+  $("#image").droppable({
+    accept: ".images",
+    drop:   function( event, ui ){
+      var MAX_INFO_FIELDS = 1;
+      var n = $( this ).find("li").length;
+      if (n < MAX_INFO_FIELDS ){
+        $("<li></li>").text( ui.draggable.text() ).appendTo( this);
+        if ( n == (MAX_INFO_FIELDS - 1)){ $(this).find("#add").remove();}
+      } // if
+    } // drop
   });
 });
